ff46c3b1394cf565640fb945c6060e55e8a821d2
yeonggoon <kty3749@gmail.com>
2020-09-11 16:59:05 +0900

    기본 홈페이지 제작

---
+++ js/ssal.js
@@ -0,0 +1,29 @@
+let headerWrap = document.getElementById('header-wrap');
+let fundRegistration = document.getElementsByClassName('fundRegistration');
+let descriptionSection = document.querySelectorAll('.description section');
+
+function gnbBtnFunction() {
+    headerWrap.classList.toggle('clicked');
+    fundRegistration[0].classList.toggle('scrollDown');
+   
+}
+function fundRegScroll(){
+    if(document.documentElement.scrollTop>20){
+        fundRegistration[0].classList.add('scrollDown');
+        console.log(1);
+    }
+    else{
+        fundRegistration[0].classList.remove('scrollDown');
+    }
+}
+
+function descriptionScroll(num,scroll){
+    if(document.documentElement.scrollTop>scroll){
+        descriptionSection[num].classList.add('active');
+    }
+}
+window.onscroll = function () {
+    fundRegScroll();
+    descriptionScroll(1,600);
+    descriptionScroll(2,1340);
+}
